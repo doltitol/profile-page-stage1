07ce1b2c149e6068bac5bc88946245ff2a297cb5
Oludolapo Adegoroye <doltil@yahoo.co.uk>
2022-10-30 00:05:36 +0100

    added profile image

--- src/components/ProfileSection/ProfileSection.jsx
+++ src/components/ProfileSection/ProfileSection.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-// import ProfileImage from '../ProfileImage/ProfileImage';
+import ProfileImage from '../ProfileImage/ProfileImage';
 // import ShareButton from '../ShareButton/ShareButton';
 import { ProfileData } from '../../assets/data/ProfileData';
 import './ProfileSection.css';
 
 const ProfileSection = () => {
-    const { twitter } = ProfileData;
+    const { profile_img, twitter } = ProfileData;
     return (
         <div className='profile-section'>
             {/* <ShareButton /> */ }
-            {/* <ProfileImage profile_img={ profile_img } alt={ twitter } /> */ }
+            <ProfileImage profile_img={ profile_img } alt={ twitter } />
             <h1 className='twitter'>{ twitter }</h1>
         </div>
     );
