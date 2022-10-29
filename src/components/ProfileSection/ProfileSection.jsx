94a9b80a0266004ed450fc8bcc520e7289120c5e
Oludolapo Adegoroye <doltil@yahoo.co.uk>
2022-10-30 00:03:38 +0100

    trying to fix profile section bug

--- src/components/ProfileSection/ProfileSection.jsx
+++ src/components/ProfileSection/ProfileSection.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import ProfileImage from '../ProfileImage/ProfileImage';
-import ShareButton from '../ShareButton/ShareButton';
+// import ProfileImage from '../ProfileImage/ProfileImage';
+// import ShareButton from '../ShareButton/ShareButton';
 import { ProfileData } from '../../assets/data/ProfileData';
 import './ProfileSection.css';
 
 const ProfileSection = () => {
-    const { profile_img, twitter } = ProfileData;
+    const { twitter } = ProfileData;
     return (
         <div className='profile-section'>
-            <ShareButton />
+            {/* <ShareButton /> */ }
             {/* <ProfileImage profile_img={ profile_img } alt={ twitter } /> */ }
             <h1 className='twitter'>{ twitter }</h1>
         </div>
