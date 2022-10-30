e631a8ecbe25e4576a696606a9f9bdbd306f72c8
Oludolapo Adegoroye <doltil@yahoo.co.uk>
2022-10-30 09:42:03 +0100

    fixed issues with links

--- src/components/LinkSection/LinkSection.jsx
+++ src/components/LinkSection/LinkSection.jsx
@@ -6,37 +6,37 @@ const LinkSection = ({ profileData }) => {
     const buttonsData = [
         {
             id: 'twitter',
-            buttonText: 'twitter link',
+            buttonText: 'Twitter Link',
             buttonUrl: `https://twitter.com/${ profileData.twitter_handle }`,
             buttonSubText: 'Twitter'
         },
         {
             id: 'btn__zuri',
-            buttonText: 'zuri team',
+            buttonText: 'Zuri Team',
             buttonUrl: 'https://training.zuri.team/',
             buttonSubText: 'Zuri team'
         },
         {
             id: 'books',
-            buttonText: 'zuri books',
+            buttonText: 'Zuri Books',
             buttonUrl: 'http://books.zuri.team',
             buttonSubText: 'Find books about design and coding'
         },
         {
             id: 'book__python',
-            buttonText: 'python books',
+            buttonText: 'Python Books',
             buttonUrl: `https://books.zuri.team/python-for-beginners?ref_id=${ profileData.slack }`,
             buttonSubText: 'Python books'
         },
         {
             id: 'pitch',
-            buttonText: 'background check for coders',
+            buttonText: 'Background Check for Coders',
             buttonUrl: `https://background.zuri.team`,
             buttonSubText: 'Where you pitch a service for doing background checks on coders'
         },
         {
             id: 'book__design',
-            buttonText: 'design books',
+            buttonText: 'Design Books',
             buttonUrl: `https://books.zuri.team/design-rules `,
             buttonSubText: 'Free design book offered by Zuri'
         }
