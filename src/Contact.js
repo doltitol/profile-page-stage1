4338c4b1a8a2119d034a8302082b9f468ab24c08
Oludolapo Adegoroye <doltil@yahoo.co.uk>
2022-11-04 20:21:34 +0100

    feat: added error message feature to input fields

--- src/Contact.js
+++ src/Contact.js
@@ -8,18 +8,33 @@ const Contact = () => {
         email: '',
         message: ''
     });
-    const [ hint, setHint ] = useState({
-        firstName: false,
-        lastName: false,
-        email: false,
-        message: false
-    });
+    const [ firstNameHint, setFirstNameHint ] = useState(false);
+    const [ lastNameHint, setLastNameHint ] = useState(false);
+    const [ emailHint, setEmailHint ] = useState(false);
+    const [ messageError, setMessageError ] = useState(false);
     const name = 'Oludolapo';
 
     const handleSubmit = (event) => {
         event.preventDefault();
         if (contactDetails.first_name === '') {
-            setHint({ ...hint, firstName: true });
+            setFirstNameHint(true);
+        } else {
+            setFirstNameHint(false);
+        }
+        if (contactDetails.last_name === '') {
+            setLastNameHint(true);
+        } else {
+            setLastNameHint(false);
+        }
+        if (contactDetails.email === '') {
+            setEmailHint(true);
+        } else {
+            setEmailHint(false);
+        }
+        if (contactDetails.message === '') {
+            setMessageError(true);
+        } else {
+            setMessageError(false);
         }
     };
 
@@ -40,27 +55,28 @@ const Contact = () => {
                         <div className='row'>
                             <div className='col-50 mr-10'>
                                 <label htmlFor='first_name'>First name</label>
-                                <input placeholder='Enter your first name' type='text' name='first_name' id='first_name' />
-                                <p className='hint' style={ { display: hint.firstName ? 'block' : 'none' } }>Please enter your first name.</p>
+                                <input placeholder='Enter your first name' type='text' name='first_name' id='first_name' onChange={ handleInputChange } />
+                                <p className='hint' style={ { display: firstNameHint ? 'block' : 'none' } }>Please enter your first name.</p>
                             </div>
                             <div className='col-50 ml-10'>
                                 <label htmlFor='last_name'>Last name</label>
-                                <input placeholder='Enter your last name' type='text' name='last_name' id='last_name' />
-                                <p className='hint' style={ { display: hint.lastName ? 'block' : 'none' } }>Please enter your last name.</p>
+                                <input placeholder='Enter your last name' type='text' name='last_name' id='last_name' onChange={ handleInputChange } />
+                                <p className='hint' style={ { display: lastNameHint ? 'block' : 'none' } }>Please enter your last name.</p>
                             </div>
                         </div>
                         <div className='row'>
                             <div className='col-100'>
                                 <label htmlFor='email'>Email</label>
-                                <input placeholder='yourname@email.com' type='email' name='email' id='email' />
-                                <p className='hint' style={ { display: hint.email ? 'block' : 'none' } }>Please enter your email address.</p>
+                                <input placeholder='yourname@email.com' type='email' name='email' id='email' onChange={ handleInputChange } />
+                                <p className='hint' style={ { display: emailHint ? 'block' : 'none' } }>Please enter your email address.</p>
                             </div>
                         </div>
                         <div className='row'>
                             <div className='col-100'>
                                 <label htmlFor='message'>Message</label>
-                                <textarea rows={ 5 } id='message' name='message' className={ hint.message ? 'error' : '' } placeholder={ `Send me a message and I'll reply you as soon as possible...` } />
-                                <p className='hint' style={ { display: hint.message ? 'block' : 'none', color: '#F83F23' } }>Please enter a message.</p>
+                                <textarea rows={ 5 } id='message' name='message' className={ messageError ? 'error' : '' } placeholder={ `Send me a message and I'll reply you as soon as possible...` }
+                                    onChange={ handleInputChange } />
+                                <p className='hint' style={ { display: messageError ? 'block' : 'none', color: '#F83F23' } }>Please enter a message.</p>
                             </div>
                         </div>
                         <div className='row'>
